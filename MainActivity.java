908b9222f2fe750b2658bcfe4e3d0bdb98e9aa68
yechanyu <128338779+yechanyu@users.noreply.github.com>
2023-04-10 20:42:57 +0900

    계산기

--- MainActivity.java
+++ MainActivity.java
@@ -1,53 +1,63 @@
-package com.example.myapplication;
+package com.example.practice;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    Button button1,button2,button3,button4;
+    float n1,n2,result;
+    TextView tv;
+    EditText a,b;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        button1=(Button)findViewById(R.id.button1);
-        button2=(Button)findViewById(R.id.button2);
-        button3=(Button)findViewById(R.id.button3);
-        button4=(Button)findViewById(R.id.button4);
+        setTitle("계산기");
 
-        button1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Uri uri = Uri.parse("http://www.naver.com");
-                Intent it  = new Intent(Intent.ACTION_VIEW,uri);
-                startActivity(it);
-            }
-        });
-        button2.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Uri uri = Uri.parse("tel:911");
-                Intent it = new Intent(Intent.ACTION_DIAL, uri);
-                startActivity(it);;
-            }
-        });
-        button3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent mIntent = new Intent(Intent.ACTION_VIEW,Uri.parse("content://media/internal/images/media"));
-                startActivity(mIntent);;
-            }
-        });
-        button4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                finish();
-            }
-        });
+        tv=(TextView)findViewById(R.id.result1);
+        a=(EditText)findViewById(R.id.text1);
+        b=(EditText)findViewById(R.id.text2);
+
+    }
+    public void addClick(View v){
+        n1=Float.parseFloat(a.getText().toString());
+        n2=Float.parseFloat(b.getText().toString());
+        result=n1+n2;
+        tv.setText(Float.toString(result));
+    }
+    public void subClick(View v){
+        n1=Float.parseFloat(a.getText().toString());
+        n2=Float.parseFloat(b.getText().toString());
+        result=n1-n2;
+        tv.setText(Float.toString(result));
+    }
+    public void divClick(View v){
+        n1=Float.parseFloat(a.getText().toString());
+        n2=Float.parseFloat(b.getText().toString());
+        if(n2==0){
+            Toast.makeText(this, "0입니다.", Toast.LENGTH_SHORT).show();
+        }
+        else {
+            result = n1 / n2;
+            tv.setText(Float.toString(result));
+        }
+    }
+    public void mulClick(View v){
+        n1=Float.parseFloat(a.getText().toString());
+        n2=Float.parseFloat(b.getText().toString());
+        result=n1*n2;
+        tv.setText(Float.toString(result));
+    }
+    public void restClick(View v){
+        n1=Float.parseFloat(a.getText().toString());
+        n2=Float.parseFloat(b.getText().toString());
+        result=n1%n2;
+        tv.setText(Float.toString(result));
     }
 }
